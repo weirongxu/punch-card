8acb2769e3335e4d2223edfbabb4ea68fa2f5eba
Weirong Xu <weirongxu.raidou@gmail.com>
2015-11-04 18:07:59 +0800

    update

--- components/marker.vue
+++ components/marker.vue
@@ -18,6 +18,30 @@
   var today = `${now.getFullYear()}-${now.getMonth()}-${now.getDay()}`;
   var cache = new Cache('marker-' + today);
 
+  function timestr2date(time_str) {
+    var time = time_str.split(':');
+    var date = new Date();
+    date.setHours(parseInt(time[0]));
+    date.setMinutes(parseInt(time[1]));
+    return date;
+  }
+
+  function date2timestr(date) {
+    return time_format({hours: date.getHours(), minutes: now.getMinutes()});
+  }
+
+  function time_format({hours=0, minutes=0}) {
+    var hour = (hours + parseInt(minutes/60)).toString();
+    var minute = parseInt(minutes%60).toString();
+    if (hour.length === 1) {
+      hour = '0' + hour;
+    }
+    if (minute.length === 1) {
+      minute = '0' + minute;
+    }
+    return `${hour}:${minute}`
+  }
+
   export default {
     data() {
       return {
@@ -26,9 +50,8 @@
     },
     methods: {
       mark() {
-        var now = new Date();
         this.marks.push({
-          time: this.format({hours: now.getHours(), minutes: now.getMinutes()}),
+          time: date2timestr(new Date()),
         });
       },
       remove(mark) {
@@ -36,32 +59,19 @@
       },
       worktime() {
         var throughMs = 0;
-        for (var i=0; i+1 < this.marks.length; i+=2) {
-          var start = this.marks[i].time.split(':');
-          var end = this.marks[i+1].time.split(':');
-          var start_date = new Date();
-          var end_date = new Date();
-          start_date.setHours(parseInt(start[0]));
-          start_date.setMinutes(parseInt(start[1]));
-          end_date.setHours(parseInt(end[0]));
-          end_date.setMinutes(parseInt(end[1]));
-          var diffMs = end_date - start_date;
+        for (var i=0; i < this.marks.length; i+=2) {
+          var start = timestr2date(this.marks[i].time);
+          if (i+1 in this.marks) {
+            var end = timestr2date(this.marks[i+1].time);
+          } else {
+            var end = new Date();
+          }
+          var diffMs = end - start;
           throughMs += diffMs;
         }
         var through_minute = throughMs / 1000 / 60;
-        return this.format({minutes: through_minute})
+        return time_format({minutes: through_minute})
       },
-      format({hours=0, minutes=0}) {
-        var hour = (hours + parseInt(minutes/60)).toString();
-        var minute = parseInt(minutes%60).toString();
-        if (hour.length === 1) {
-          hour = '0' + hour;
-        }
-        if (minute.length === 1) {
-          minute = '0' + minute;
-        }
-        return `${hour}:${minute}`
-      }
     },
     watch: {
       'marks': function(val, old_val){
--- dist/app.js
+++ dist/app.js
@@ -10318,6 +10318,35 @@
 	var today = now.getFullYear() + '-' + now.getMonth() + '-' + now.getDay();
 	var cache = new _cache2['default']('marker-' + today);
 
+	function timestr2date(time_str) {
+	  var time = time_str.split(':');
+	  var date = new Date();
+	  date.setHours(parseInt(time[0]));
+	  date.setMinutes(parseInt(time[1]));
+	  return date;
+	}
+
+	function date2timestr(date) {
+	  return time_format({ hours: date.getHours(), minutes: now.getMinutes() });
+	}
+
+	function time_format(_ref) {
+	  var _ref$hours = _ref.hours;
+	  var hours = _ref$hours === undefined ? 0 : _ref$hours;
+	  var _ref$minutes = _ref.minutes;
+	  var minutes = _ref$minutes === undefined ? 0 : _ref$minutes;
+
+	  var hour = (hours + parseInt(minutes / 60)).toString();
+	  var minute = parseInt(minutes % 60).toString();
+	  if (hour.length === 1) {
+	    hour = '0' + hour;
+	  }
+	  if (minute.length === 1) {
+	    minute = '0' + minute;
+	  }
+	  return hour + ':' + minute;
+	}
+
 	exports['default'] = {
 	  data: function data() {
 	    return {
@@ -10326,9 +10355,8 @@
 	  },
 	  methods: {
 	    mark: function mark() {
-	      var now = new Date();
 	      this.marks.push({
-	        time: this.format({ hours: now.getHours(), minutes: now.getMinutes() })
+	        time: date2timestr(new Date())
 	      });
 	    },
 	    remove: function remove(mark) {
@@ -10336,36 +10364,18 @@
 	    },
 	    worktime: function worktime() {
 	      var throughMs = 0;
-	      for (var i = 0; i + 1 < this.marks.length; i += 2) {
-	        var start = this.marks[i].time.split(':');
-	        var end = this.marks[i + 1].time.split(':');
-	        var start_date = new Date();
-	        var end_date = new Date();
-	        start_date.setHours(parseInt(start[0]));
-	        start_date.setMinutes(parseInt(start[1]));
-	        end_date.setHours(parseInt(end[0]));
-	        end_date.setMinutes(parseInt(end[1]));
-	        var diffMs = end_date - start_date;
+	      for (var i = 0; i < this.marks.length; i += 2) {
+	        var start = timestr2date(this.marks[i].time);
+	        if (i + 1 in this.marks) {
+	          var end = timestr2date(this.marks[i + 1].time);
+	        } else {
+	          var end = new Date();
+	        }
+	        var diffMs = end - start;
 	        throughMs += diffMs;
 	      }
 	      var through_minute = throughMs / 1000 / 60;
-	      return this.format({ minutes: through_minute });
-	    },
-	    format: function format(_ref) {
-	      var _ref$hours = _ref.hours;
-	      var hours = _ref$hours === undefined ? 0 : _ref$hours;
-	      var _ref$minutes = _ref.minutes;
-	      var minutes = _ref$minutes === undefined ? 0 : _ref$minutes;
-
-	      var hour = (hours + parseInt(minutes / 60)).toString();
-	      var minute = parseInt(minutes % 60).toString();
-	      if (hour.length === 1) {
-	        hour = '0' + hour;
-	      }
-	      if (minute.length === 1) {
-	        minute = '0' + minute;
-	      }
-	      return hour + ':' + minute;
+	      return time_format({ minutes: through_minute });
 	    }
 	  },
 	  watch: {
