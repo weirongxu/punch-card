6259e77b0b4e3cb5778a76c3b80d9f25d4eb748d
Weirong Xu <weirongxu.raidou@gmail.com>
2016-02-22 17:09:43 +0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,31 +5,14 @@ var browserSync = require('browser-sync').create();
 var runSequence = require('run-sequence');
 var inlineSource = require('gulp-inline-source');
 
-function webpack_src() {
-  return gulp.src('index.js')
-  .pipe(webpack(require('./webpack.config.js')))
-}
-
-gulp.task('webpack', function(){
+// dev
+gulp.task('dev.webpack', () => {
   return gulp.src('index.js')
   .pipe(webpack(require('./webpack.config.js')))
   .pipe(gulp.dest('./assets'));
 });
 
-gulp.task('webpack.build', function(){
-  return gulp.src('index.js')
-  .pipe(webpack(require('./webpack.build.config.js')))
-  .pipe(uglify())
-  .pipe(gulp.dest('./build/assets'));
-});
-
-gulp.task('html.build', function(){
-  return gulp.src('index.html')
-  .pipe(inlineSource())
-  .pipe(gulp.dest('./build'));
-});
-
-gulp.task('browser-sync', function(){
+gulp.task('dev.browser-sync', () => {
   return browserSync.init({
     files: [
       'assets/**',
@@ -42,8 +25,27 @@ gulp.task('browser-sync', function(){
   });
 });
 
-gulp.task('default', ['webpack', 'browser-sync']);
+gulp.task('dev', ['dev.webpack', 'dev.browser-sync']);
 
-gulp.task('build', function(){
-  runSequence('webpack.build', 'html.build');
+// build
+gulp.task('build.webpack', () => {
+  return gulp.src('index.js')
+  .pipe(webpack(require('./webpack.build.config.js')))
+  .pipe(uglify())
+  .pipe(gulp.dest('./build/assets'));
 });
+
+gulp.task('build.html', () => {
+  return gulp.src('index.html')
+  .pipe(gulp.dest('./build'));
+});
+
+gulp.task('build.inline-html', () => {
+  return gulp.src('build/index.html')
+  .pipe(inlineSource())
+  .pipe(gulp.dest('./build-inline'));
+});
+
+gulp.task('build', () => runSequence('build.webpack', 'build.html') );
+
+gulp.task('build-inline', () => runSequence('build.webpack', 'build.html', 'build.inline-html') );
